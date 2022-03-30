b572bd87e25fa3e54a5d0acd8c2bc38528d5b1aa
Snigdha Motadaka <40993241+snigui@users.noreply.github.com>
2022-03-30 11:58:25 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -9,13 +9,10 @@ app = Flask(__name__, static_url_path='/static')
 def main():
     return render_template("index.html")
 
-def run():
-    app.run(host="0.0.0.0", port=8888)
-
 
 def keep_alive():
     server = Thread(target=run)
     server.start()
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0', port=5000)
