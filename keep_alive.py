534e371e733c374037aacca4aa2c44c8cd3c317c
Snigdha Motadaka <40993241+snigui@users.noreply.github.com>
2022-03-30 11:30:14 -0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,13 +1,12 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__, static_url_path='/static')
 
 
 @app.route('/')
 def main():
-    return "Your bot is alive!"
-
+    return render_template("index.html")
 
 def run():
     app.run(host="0.0.0.0", port=8888)
