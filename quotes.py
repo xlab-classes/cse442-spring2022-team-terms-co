8a73e2555f5fc9e1909c35140ce5b00e9dc56811
Elijah Washington <45720104+Eliwash73@users.noreply.github.com>
2022-04-11 16:19:39 -0400

    quotes.py added

---
+++ quotes.py
@@ -0,0 +1,50 @@
+cliche= [
+        "I have not failed. I've just found 10,000 ways that won't work.\n-Thomas Edison.",
+        "Reach for the moon. If you miss, at least you'll be among the stars.\n-Norman Vincent Peale.",
+        "I'm selfish, impatient and a little insecure.\nI make mistakes, I am out of control and at times hard to handle.\nBut if you can't handle me at my worst, then you sure as hell don't deserve me at my best.\n-Marilyn Monroe.",
+        "It does not matter how slowly you go as long as you do not stop.\n-Confucius.",
+        "The best way to get started is to quit talking and begin doing.\n-Walt Disney.",
+        "Life is about making an impact, not about making an income.\n-Kevin Kruse.",
+        "First, have a definite, clear practical ideal; a goal, an objective.\nSecond, have the necessary means to achieve your ends; wisdom, money, materials, and methods.\nThird, adjust all your means to that end.\n-Aristotle.",
+        "Do the best you can until you know better. Then when you know better, do better.\n-Maya Angelou.",
+        "In three words I can sum up everything I've learned about life: it goes on.\n-Robert Frost.",
+        "Failure is simply the opportunity to begin again,\nthis time more intelligently.\n-Henry Ford.",
+        "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do.\n-Mark Twain.",
+        "Success is never owned. It is only rented, and the rent is due every day.\n-Rory Vaden.", 
+        "If life gives you lemons, make lemonade.\n-Dale Carnegie.",
+        "The important thing is not to stop questioning.\nCuriosity has its own reason for existence.\nOne cannot help but be in awe when he contemplates the mysteries of\n eternity, of life, of the marvelous structure of reality.\n-Albert Einstein.",
+        "There are only two ways to live your life.\nOne is as though nothing is a miracle.\nThe other is as though everything is a miracle.\n-Albert Einstein.",
+        ]
+
+
+motivational = [
+                "We cannot solve problems with the kind of thinking we employed when we came up with them.\n—Albert Einstein",
+                "Learn as if you will live forever, live like you will die tomorrow.\n—Mahatma Gandhi",
+                "Stay away from those people who try to disparage your ambitions.\nSmall minds will always do that, but great minds will give you a feeling that you can become great too.\n—Mark Twain",
+                "When you give joy to other people, you get more joy in return.\nYou should give a good thought to happiness that you can give out.\n—Eleanor Roosevelt",
+                "When you change your thoughts,\nremember to also change your world.\n—Norman Vincent Peale",
+                "It is only when we take chances, when our lives improve.\nThe initial and the most difficult risk that we need to take is to become honest.\n—Walter Anderson",
+                "Nature has given us all the pieces required to achieve exceptional wellness and health,\nbut has left it to us to put these pieces together.\n—Diane McLaren"
+                "None of us can see the future,\nForget it whatever's gon' happen gon' happen\n-Aha Gazelle"
+                "Thought I was finished but I'm too resilient, to keep it a trillion I'm just getting started\n-Aha Gazelle"
+                "Don't take this time that you have on this planet for granted,\nYou rushin' to die, What's the hurry? I'm tryna age like Bron Bron,\nBetter with time like fine wine, Making the most of my prime\n-Aha Gazelle"
+                ]
+
+funny = [
+        "If I don't have some cake soon, I might die.\n-Stanley Hudson, The Office Season 4, 'Survivor Man'",
+        "Identity theft is not a joke, Jim! Millions of families suffer every year.\n-Dwight Schrute, The Office Season 3, 'Product Recall'",
+        "I am running away from my responsibilities. And it feels good.\n-Michael Scott, The Office Season 4, 'Money'",
+        "And I knew exactly what to do. But in a much more real sense, I had no idea what to do.\n-Michael Scott, The Office Season 5, 'Stress Relief'",
+        "I just want to lie on the beach and eat hot dogs.\nThat's all I've ever wanted.\n-Kevin Malone, The Office Season 3, 'Beach Games'",
+        "I just wanna get drunk and eat chicken fingers.\n-Trailer Park Boys. Season 1, Episode 2",
+        "How can a peanut kill someone? It's not even a person.\n-Trailer Park Boys. Season 8, Episode 7",
+        "I guess we've learned that no matter who you are or where you come from, life is a terrible thing.\n-Peter Griffin, Family Guy",
+        "What? Gosh, it's not like the internet to go crazy about something small and stupid.\n-Peter Griffin, Family Guy",
+        "I am absolutely paralyzed by decision-making.\n-Chidi Anagonye, 'The Good Place'"
+        "The only thing I'm not good at is modesty, because I'm great at it.\n-Gina Linette, 'Brooklyn Nine-Nine'."
+        ]
+
+
+casual = ["Yoooo you gotta ", 'ight time to ', "Bro don't forget to ", 'yeeerrrooo ' ]
+motivate = ['You got this! Time to ', 'Rise and Grind. Time to ']
+normal = ['Make sure to ', "Hey, don't forget to ", "It's time to ", "I'd like to remind you to ", "I hope you haven't forgotten to ", "May I remind you to ", "Friendly reminder to "]
